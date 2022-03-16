693f64d6f85578658d1b964b7f4f118529e7ebeb
Carlos Borba <cmachado525@gmail.com>
2022-03-16 12:16:25 -0300

    Start project

---
+++ src/main/java/com/colab/doginder/DoginderApplication.java
@@ -0,0 +1,13 @@
+package com.colab.doginder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoginderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoginderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colab/doginder/DoginderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colab.doginder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoginderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
